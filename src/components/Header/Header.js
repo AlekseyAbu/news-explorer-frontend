8b3b46209fa48f3cbadfffd64961179141a2724b
Aleksey Abu <direct.abu@yandex.ru>
2021-01-15 20:52:10 +0300

    6

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,6 @@ import { useLocation } from 'react-router-dom';
 
 function Header(props) {
     const location = useLocation();
-    console.log(location.pathname)
     const path = location.pathname;
 
     
@@ -11,11 +10,15 @@ function Header(props) {
     return(
         <header className={`header ${path !== '/saved-news' ? '' : 'header_black' }`}>
             <h2 className='header__title'>NewsExplorer</h2>
+            <input id='checkbox' className='header__button_checkbox' type='checkbox'/>
+            <label for='checkbox' className='header__button_mobail'></label>
+            {/* <div className='header__buttons'> */}
             <nav className='header__navigation'>
                 <a className='header__nav header__nav_active'>Главная</a>
                 <a className='header__nav'>Сохранённые статьи</a>
             </nav>
             <button className={`header__button ${path !== '/saved-news' ? '' : 'header__button_black'}`}>Авторизоваться</button>
+            {/* </div> */}
         </header>
     )
 }
